faff6cca069b1132f9cfebf6224eb3ca2647d580
guccisquad <unknowneg@yandex.ru>
2017-10-28 13:17:49 +0300

    update js

---
+++ lw3/scripts/registration.js
@@ -0,0 +1,38 @@
+function registration(){
+  var userEmail = document.getElementById('email');
+  var userPassword = document.getElementById('password');
+  var userPasswordAgain = document.getElementById('passwordAgain');
+  var userCheckBox = document.getElementById('rules');    
+  var checkMailErrors = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(userEmail.value));
+
+validate = function(){ 
+  if (userEmail.value == "" || userEmail.value == " " || userEmail.value.length < 6){
+    alert('Неправильно введен Email');
+  }
+    else{
+      if (userPassword.value == "" || userPassword.value == " " || userPassword.value.length < 6){
+        alert('Неправильно введен пароль');
+      }
+      else{
+        if (userPasswordAgain.value == "" || userPasswordAgain.value == " " || userPasswordAgain.value != userPassword.value){
+         alert('Пороли не совпадают');
+       }
+        else{
+          if (!userCheckBox.checked){
+            alert('Согласитесь с правилами');
+          }
+          else{
+            alert('Регистрация завершена');
+          }
+        }
+      }
+    }
+  }
+
+validate();
+
+}
+
+window.onload = function() {
+  document.getElementById('registration').addEventListener('submit', registration);
+}
