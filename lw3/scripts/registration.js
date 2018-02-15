50ab53c685149aae8b869c3f2236af501f9c0aef
guccisquad <31971866+guccisquad@users.noreply.github.com>
2018-02-15 22:17:02 +0300

    fix issues

--- lw3/scripts/registration.js
+++ lw3/scripts/registration.js
@@ -1,41 +1,34 @@
-function validateRegistration(event){
-  event.preventDefault();
+function validateRegistration(){
   var userEmail = document.getElementById('email');
   var userPassword = document.getElementById('password');
   var userPasswordAgain = document.getElementById('passwordAgain');
-  var userCheckBox = document.getElementById('rules');    
+  var userCheckBox = document.getElementById('rules');
   var checkMailErrors = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(userEmail.value));
 
-validate = function(){ 
   if (userEmail.value == "" || userEmail.value == " " || userEmail.value.length < 6){
     alert('Неправильно введен Email');
+  } else if (userPassword.value == "" || userPassword.value == " " || userPassword.value.length < 6){
+      alert('Неправильно введен пароль');
+  } else if (userPasswordAgain.value == "" || userPasswordAgain.value == " " || userPasswordAgain.value != userPassword.value){
+     alert('Пороли не совпадают');
+  } else if (!userCheckBox.checked){
+      alert('Согласитесь с правилами');
+  } else {
+    alert('Регистрация завершена');
   }
-    else{
-      if (userPassword.value == "" || userPassword.value == " " || userPassword.value.length < 6){
-        alert('Неправильно введен пароль');
-        return;
-      }
-      else{
-        if (userPasswordAgain.value == "" || userPasswordAgain.value == " " || userPasswordAgain.value != userPassword.value){
-         alert('Пороли не совпадают');
-       }
-        else{
-          if (!userCheckBox.checked){
-            alert('Согласитесь с правилами');
-          }
-          else{
-            alert('Регистрация завершена');
-          }
-        }
-      }
-    }
-  }
-
-validate();
-
+  return true;
 }
 
+function registration(event) {
+  event.preventDefault();
+
+  var validateStatus = validateRegistration();
+  if (!validateStatus) {
+      alert(validateStatus);
+     return;
+  }
+}
 
 window.onload = function() {
-  document.getElementById('registration').addEventListener('submit', validateRegistration);
+  document.getElementById('registration').addEventListener('submit', registration);
 }
