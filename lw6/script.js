27a571f279ba5fd0d7bf4b1a837e3fb44874f4e2
guccisquad <unknowneg@yandex.ru>
2018-03-03 15:06:34 +0300

    fix issues

--- lw6/script.js
+++ lw6/script.js
@@ -1,41 +1,43 @@
 class CoffeeMachine {
-  constructor(moneyCash, coffeeMenu, currentCoffeeNumber) {
+  constructor() {
     this.moneyCash = 0;
     this.coffeeMenu = [{id: 1, name: 'Американо', price: 10 },
                       {id: 2, name: 'Капучино', price: 15},
-                      {id: 3, name: 'Латте', price: 20}];
-    this.currentCoffeeNumber = currentCoffeeNumber;
+                      {id: 3, name: 'Латте', price: 20},
+                      {id: 4, name: '123', price: 510}];
+    this.currentCoffeeNumber = 0;
   }
 
   setCash(money) {
-    if ((money == 1) || (money == 2) || (money == 5) || (money == 10) || (money == 50)) {
+    if ((money === 1) || (money === 2) || (money === 5) || (money === 10) || (money === 50)) {
       this.moneyCash += money;
-      console.log('Ваш баланс: ' + this.moneyCash + ' руб.')
+      console.log(`Ваш баланс: ${this.moneyCash} руб.`)
       return true;
-    } else
-      console.log('Аппарат принимает монеты 1, 2, 5, 10 руб. и купюры 10 и 50 руб.')
+    } else {
+      console.log(`Аппарат принимает монеты 1, 2, 5, 10 руб. и купюры 10 и 50 руб.`)
       return false;
+    }
   }
 
   getCoffeeMenu() {
     this.coffeeMenu.forEach(function(cup, i) {
-      console.log(cup.id + '-' + cup.name + '(' + cup.price + 'руб.)');
+      console.log(`${cup.id} - ${cup.name} ${cup.price}  руб`);
     });
   }
 
   checkNumber(num) {
-    if ((num < 1 ) || (num > 3)) {
+    if ((num < 1) && (num >= this.coffeeMenu.length))
       return false;
-    } else
+    else
       return true;
-    }
+  }
 
   checkMoney(coffeeNumber) {
-    if (this.moneyCash < this.coffeeMenu[coffeeNumber].price) {
+    if (this.moneyCash < this.coffeeMenu[coffeeNumber].price)
       return false;
-    } else
+    else
       return true;
-    }
+  }
 
   chooseCoffee(coffeeNumber) {
     if (this.checkNumber(coffeeNumber) && this.checkMoney(coffeeNumber - 1)) {
@@ -43,10 +45,9 @@ class CoffeeMachine {
       return true;
     } else
       return false;
-    }
+  }
 
   getRemainCash(remain) {
-    remain = this.moneyCash - this.coffeeMenu[this.currentCoffeeNumber - 1].price;
-    console.log('Ваша сдача: ' + remain + 'руб.');
+    return remain = this.moneyCash - this.coffeeMenu[this.currentCoffeeNumber - 1].price;
   }
 }
